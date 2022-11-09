8f0e0e6fe0204793a760e5f5884662df35cf4d8d
LucasFromDK <lucashjerbechlarsen@gmail.com>
2022-11-09 14:21:01 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 let input;
 let Saldo = ""
 
-function preload() {
 
-}
+function preload() {}
 
 function setup() {
-  StartSaldo()
   Buttons()
+  StartSaldo()
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background("200");
-  FrontEnd()
+  FrontEnd();
   text("Konto: " + Saldo + " DKK", 5, 45)
 }
 
@@ -35,28 +34,28 @@ function StartSaldo(){
 
   //Submit Knap
   button = createButton('Submit');
-  button.size(75, 25)
+  button.size(75, 25);
   button.position(input.width + 5, 50);
   button.mousePressed(setSaldo);
 }
 
 function setSaldo() {
   //Tager og sætter input til Saldo
-  Saldo = input.value()
+  Saldo = input.value();
 }
 
 function FrontEnd() {
-  TopBar()
-  fill("Black")
+  TopBar();
+  fill("Black");
 }
 
 function TopBar() {
   fill ("#41C0f2");
   rect(0, 0, width, 30);
-  fill("black")
-  textSize(20)
-  text("Lukato Budget | Lukato Group", 5, 22)
-  text("❌",windowWidth - 30, 22)
+  fill("black");
+  textSize(20);
+  text("Lukato Budget | Lukato Group", 5, 22);
+  text("❌",windowWidth - 30, 22);
   //Default Text Size
-  textSize(12)
+  textSize(12);
 }
